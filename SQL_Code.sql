fe3c12816058e420f8e7ad4ed5a8637057aa4da8
Toni Gonzalez gomez <toni.b.gonzalez.gomez-1@ou.edu>
2026-02-15 21:57:10 -0600

    Update and rename FinalDB.sql to SQL_Code.sql

--- SQL_Code.sql
+++ SQL_Code.sql
@@ -1,3 +1,6 @@
+-- =====================================
+-- Database Creation
+-- =====================================
 CREATE DATABASE hospital16;
 USE hospital16;
 
@@ -7,7 +10,9 @@ FROM patients
 LEFT JOIN visits
 ON patients.PatientID = visits.PatientID
 WHERE visits.`Admission Type` = "Emergency";
-
+-- =====================================
+-- Analytical Queries
+-- =====================================
 
 SELECT billing.VisitID, billing.Doctor, billing.Hospital, billing.`Insurance Provider`, 
 sum(billing.`Billing Amount`) as debt, visits.VisitID, visits.PatientID, visits.`Admission Type`
@@ -36,12 +41,19 @@ LEFT JOIN visits
 ON billing.VisitID = visits.VisitID
 WHERE billing.`Insurance Provider` IN ("UnitedHealthcare", "Blue Cross", "Medicare");
 
-DROP PROCEDURE IF EXISTS DateBetween$$
 
--- Part 2
+-- =====================================
+-- Stored Procedures
+-- =====================================
+USE hospital16;
+DROP PROCEDURE IF EXISTS highDebt;
+
+DROP PROCEDURE IF EXISTS DateBetween$$;
+
+
 DELIMITER $$
 USE `hospital16`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `DateBetween`()
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DatesBetween`()
 BEGIN
 SELECT patients.Name, patients.PatientID, patients.Age, patients.Gender, patients.`Medical Condition`,
 visits.`Date of Admission`, visits.VisitID,
@@ -54,20 +66,55 @@ ON visits.VisitID=billing.VisitID
 WHERE visits.`Date of Admission` BETWEEN '2019-01-01' AND '2021-12-31';
 END$$
 DELIMITER ;
-CALL DateBetween();
+CALL DatesBetween();
 
 
 DELIMITER $$
-USE `week13`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `leftJoin2`()
+USE `hospital16`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `highDebt`()
 BEGIN
-SELECT Name, Doctor, SUM(InvoiceAMT) AS TotalAMT
+SELECT patients.Name, patients.PatientID, patients.Age, patients.Gender, patients.`Medical Condition`,
+visits.`Date of Admission`, visits.VisitID, billing.`Billing Amount`,
+billing.VisitID
 FROM patients
-LEFT JOIN week13_visit
-ON week13_patient.PatientID=week13_visit.PatientID
-LEFT JOIN week13_invoice
-ON week13_visit.visitID=week13_invoice.visitID
-WHERE TotalAmt > 1000;
+LEFT JOIN visits
+ON patients.PatientID=visits.PatientID
+LEFT JOIN billing
+ON visits.VisitID=billing.VisitID
+WHERE billing.`Billing Amount` > 1000;
 END$$
 DELIMITER ;
-CALL leftJoin2();
+CALL highDebt();
+
+-- =====================================
+-- Triggers
+-- =====================================
+
+DELIMITER //
+CREATE TRIGGER ErrorMessage
+BEFORE INSERT ON patients FOR EACH ROW
+BEGIN
+IF NEW.Age <18 
+THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT= 'Patient is Not a Legal Adult';
+END IF;
+END;
+//
+SHOW TRIGGERS;
+
+INSERT INTO patients (Name, PatientID, Age, Gender)
+VALUES ('YTony', '09877654', '16', 'Male');
+
+DROP TRIGGER IF EXISTS NewInsurance;
+
+
+DELIMITER //
+CREATE TRIGGER NewInsurance
+BEFORE INSERT ON billing
+FOR EACH ROW
+IF NEW.`Insurance Provider` = "Aetna" THEN SET NEW.`Insurance Provider` = "Obama Care";
+END IF;//
+
+SELECT * FROM billing;
+SHOW TRIGGERS;
